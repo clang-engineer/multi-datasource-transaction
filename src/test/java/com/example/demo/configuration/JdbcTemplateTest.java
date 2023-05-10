264270286845159e8fe8b496d8a9b4fac5231309
yorez <yorez@naver.com>
2023-05-10 16:42:12 +0900

    chore

--- src/test/java/com/example/demo/configuration/JdbcTemplateTest.java
+++ src/test/java/com/example/demo/configuration/JdbcTemplateTest.java
@@ -6,11 +6,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,20 +28,21 @@ public class JdbcTemplateTest {
     @BeforeEach
     public void setUp() {
         primaryJdbcTemplate.execute("INSERT INTO test_table VALUES (1, 'test1')");
-        secondaryJdbcTemplate.execute("INSERT INTO test_table VALUES (1, 'test1')");
     }
 
     @Test
+    @Transactional
     public void testPrimaryJdbcTemplate() {
         String query = "SELECT COUNT(*) FROM test_table";
         int result = primaryJdbcTemplate.queryForObject(query, Integer.class);
-        assertThat(result).isEqualTo(10);
+        assertThat(result).isEqualTo(1);
     }
 
     @Test
+    @Transactional
     public void testSecondaryJdbcTemplate() {
         String query = "SELECT COUNT(*) FROM test_table";
         int result = secondaryJdbcTemplate.queryForObject(query, Integer.class);
-        assertThat(result).isEqualTo(20);
+        assertThat(result).isEqualTo(1);
     }
 }
--- src/test/resources/create_table.sql
+++ src/test/resources/create_table.sql
@@ -1,5 +1,5 @@
 drop table if exists test_table;
 create table test_table (
     id int,
-    title varchar(255),
+    title varchar(255)
 );
